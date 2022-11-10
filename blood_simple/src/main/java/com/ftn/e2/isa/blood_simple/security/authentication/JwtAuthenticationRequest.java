cbc60c8f4dda1abb14afcfe816c69f557a05d6af
laslo-uri-ftn <laslo.uri@uns.ac.rs>
2022-11-10 12:35:47 +0300

    feat: Add JWT Authentication files - Security

---
+++ blood_simple/src/main/java/com/ftn/e2/isa/blood_simple/security/authentication/JwtAuthenticationRequest.java
@@ -0,0 +1,34 @@
+package com.ftn.e2.isa.blood_simple.security.authentication;
+
+public class JwtAuthenticationRequest {
+
+    private String email;
+    private String password;
+
+    public JwtAuthenticationRequest() {
+        super();
+    }
+
+    public JwtAuthenticationRequest(String email, String password) {
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
