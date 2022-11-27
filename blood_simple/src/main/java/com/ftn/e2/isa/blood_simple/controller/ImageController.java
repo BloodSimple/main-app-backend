8c6721434c31f62701f8a2488f86cc6d8479640e
markovic-dusan <shandusukisuki3@gmail.com>
2022-11-27 20:52:38 +0100

    feat[3.19]: Image model, service and controller; QR code service and controller POSTMAN tested

---
+++ blood_simple/src/main/java/com/ftn/e2/isa/blood_simple/controller/ImageController.java
@@ -0,0 +1,60 @@
+package com.ftn.e2.isa.blood_simple.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ftn.e2.isa.blood_simple.model.Image;
+import com.ftn.e2.isa.blood_simple.repository.ImageRepository;
+
+import utils.ImageUtility;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin() 
+public class ImageController {
+
+    @Autowired
+    ImageRepository imageRepository;
+
+    @PostMapping("/upload/image")
+    public ResponseEntity<String> uplaodImage(@RequestParam("image") MultipartFile file)
+            throws IOException {
+
+        imageRepository.save(Image.builder()
+                .name(file.getOriginalFilename())
+                .type(file.getContentType())
+                .image(ImageUtility.compressImage(file.getBytes())).build());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new String("Image uploaded successfully: " +
+                        file.getOriginalFilename()));
+    }
+
+//    @GetMapping(path = {"/get/image/info/{name}"})
+//    public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+//
+//        final Optional<Image> dbImage = imageRepository.findByName(name);
+//
+//        return Image.builder()
+//                .name(dbImage.get().getName())
+//                .type(dbImage.get().getType())
+//                .image(ImageUtility.decompressImage(dbImage.get().getImage())).build();
+//    }
+
+    @GetMapping(path = {"/get/image/{name}"})
+    public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+
+        final Image dbImage = imageRepository.getByName(name);
+        
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.valueOf(dbImage.getType()))
+                .body(ImageUtility.decompressImage(dbImage.getImage()));
+    }
+}
---
+++ blood_simple/src/main/java/com/ftn/e2/isa/blood_simple/controller/QrController.java
@@ -0,0 +1,39 @@
+package com.ftn.e2.isa.blood_simple.controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ftn.e2.isa.blood_simple.model.Image;
+import com.ftn.e2.isa.blood_simple.repository.ImageRepository;
+import com.ftn.e2.isa.blood_simple.service.QrService;
+
+import utils.ImageUtility;
+
+@RestController
+@RequestMapping(value="api/read-qr")
+public class QrController {
+	
+	@Autowired
+    private QrService qrCodeService;
+	@Autowired
+	ImageRepository imageRepo;
+	 @GetMapping(path = {"/{name}"})
+	    public ResponseEntity<String> getAppointmentViaQR(@PathVariable("name") String name) throws IOException {
+		 
+		 
+		 Image dbImage = imageRepo.getByName(name);
+		 String content = qrCodeService.readQRCode(ImageUtility.decompressImage(dbImage.getImage()));
+		 return new ResponseEntity<>(content, HttpStatus.OK);
+	 }
+}
