04b26bce35aa20ae0fd0d07f9e15deaaf440d4f6
markovic-dusan <shandusukisuki3@gmail.com>
2022-12-17 18:14:17 +0100

    feat[3.5]: authorization fix

--- blood_simple/src/main/java/com/ftn/e2/isa/blood_simple/config/WebSecurityConfig.java
+++ blood_simple/src/main/java/com/ftn/e2/isa/blood_simple/config/WebSecurityConfig.java
@@ -86,7 +86,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // svim korisnicima dopusti da pristupe sledecim putanjama:
                 .authorizeRequests()      // /auth/**
                 //.antMatchers("/api/**").permitAll()
-                //.antMatchers("/api/sysadmin**").access("hasRole('SYSTEM_ADMIN')")
+                .antMatchers("/api/sysadmin/**").access("hasRole('SYSTEM_ADMIN')")
+                .antMatchers("/api/centers/").access("hasRole('SYSTEM_ADMIN')")
                
                 // ukoliko ne zelimo da koristimo @PreAuthorize anotacije nad metodama kontrolera, moze se iskoristiti hasRole() metoda da se ogranici
                 // koji tip korisnika moze da pristupi odgovarajucoj ruti. Npr. ukoliko zelimo da definisemo da ruti 'admin' moze da pristupi
